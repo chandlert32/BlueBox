ca9740724a3c28dc80017178899afbbabf4df4e8
Chandler Tan <tanchandler4@gmail.com>
2020-03-31 16:11:32 -0400

    Add project files.

---
+++ RentAMovie.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentAMovie.WebMVC.Startup))]
+namespace RentAMovie.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
